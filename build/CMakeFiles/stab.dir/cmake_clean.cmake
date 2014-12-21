ce445bdf1d8aa6a156f1b2cfc7794f117abccff7
dudekw <wojciechsbox@gmail.com>
2014-12-21 12:37:18 +0100

    structure

---
+++ build/CMakeFiles/stab.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/stab.dir/src/stab.cpp.o"
+  "stab.pdb"
+  "stab"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/stab.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ src/stab.cpp
@@ -0,0 +1,12 @@
+#include <opencv2/imgproc/imgproc.hpp>
+#include "opencv2/highgui/highgui.hpp"
+#include "cv.h"
+#include <iostream>
+using namespace std;
+int main(int argc, char** argv)
+{
+
+    cout<<"test"<< endl;
+  return 0;
+}
+
